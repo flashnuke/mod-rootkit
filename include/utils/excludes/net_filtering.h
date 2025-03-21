641ddb4d54e47b19c2e964a08f67d6578f9d7d69
flashnuke <59119926+flashnuke@users.noreply.github.com>
2025-03-21 20:03:14 +0200

    Update net_filtering.h

--- include/utils/excludes/net_filtering.h
+++ include/utils/excludes/net_filtering.h
@@ -5,7 +5,7 @@
 #include <linux/inet.h>
 #include <linux/string.h>
 
-extern bool net_exclusions_are_set();
+extern int net_exclusions_are_empty();
 extern void ip_to_hex(const char *ip_str, char *hex_buf);
 extern void port_to_hex(unsigned int port, char *hex_buf);
 extern int should_exclude_line(const char *line);
