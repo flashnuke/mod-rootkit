51e6a166e93449705696004057024d49e52a0037
flashnuke <59119926+flashnuke@users.noreply.github.com>
2025-03-21 19:56:50 +0200

    Update and rename ip_filtering.h to net_filtering.h

--- include/utils/excludes/net_filtering.h
+++ include/utils/excludes/net_filtering.h
@@ -1,14 +1,15 @@
-#ifndef IP_FILTERING_H
-#define IP_FILTERING_H
+#ifndef NET_FILTERING_H
+#define NET_FILTERING_H
 
 #include <linux/in.h>
 #include <linux/inet.h>
 #include <linux/string.h>
 
+extern bool net_exclusions_are_set();
 extern void ip_to_hex(const char *ip_str, char *hex_buf);
 extern void port_to_hex(unsigned int port, char *hex_buf);
 extern int should_exclude_line(const char *line);
 extern char *filter_netstat_lines(const char *buf, size_t *new_len);
 
-#endif // IP_FILTERING_H
+#endif // NET_FILTERING_H
 
